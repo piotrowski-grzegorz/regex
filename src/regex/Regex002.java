a587727529e165eddf6e24e847f41993249144d2
Grzegorz Piotrowski <gpiotrowskiarch@gmail.com>
2023-09-26 19:35:33 +0200

    Refactored Regex001

---
+++ src/regex/Regex002.java
@@ -0,0 +1,14 @@
+package regex;
+
+public class Regex002 {
+    public static void main(String[] args) {
+        RegexTester tester = new RegexTester("[A-Z][a-z]+");
+        tester.validate("Java");//true
+        tester.validate("Ja");//true
+        tester.validate("java");//false
+        tester.validate("javA");//false
+        tester.validate("JAVa");//false
+        tester.validate("J");//false
+        tester.validate("aJ");//false
+    }
+}
